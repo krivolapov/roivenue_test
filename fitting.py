51668e9aa0faac99a2ed9f7cf778b0539a1ece71
Maksim Krivolapov <givago@mail.ru>
2020-12-29 23:45:25 +0100

    fitting

--- fitting.py
+++ fitting.py
@@ -7,13 +7,16 @@ Created on Mon Dec 28 12:37:42 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-a = 2.0
-b = 5.0
+a = 2.29
+b = 1.0
 x = np.arange(0, 10, 0.1)
 
 def function(x):
-    return a*(1 - np.e**(-(x/b)))
+    return a*(1 - np.exp(-(x/b)))
     
 y = function(x)
 
 plt.plot(x,y)
+
+aaa = np.linspace(start = -10,stop =  10, num = 25)
+yyy= np.arange(start = -1, stop =  1, step = 0.1)
