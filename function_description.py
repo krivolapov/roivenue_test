856c632ec8c7b43811671cc25fadc3f5d3cad5b2
Maksim Krivolapov <givago@mail.ru>
2021-01-26 00:04:02 +0100

    all clients and all Adds report finished

---
+++ function_description.py
@@ -0,0 +1,18 @@
+def estimator(df, function):
+	"""
+	Return dataframe with correlation's coeffitients for dataset and function
+	"""
+	pass
+
+
+def predictor(df, coeff, function):
+	pass
+
+
+def optimaizer(df, constrains):
+	"""
+	Function for optimization of list of functions. 
+	Input: df-> correlation coefficients and current values , list of constrains
+	Output: df with additional values of optimal points for next step
+	"""
+	pass
