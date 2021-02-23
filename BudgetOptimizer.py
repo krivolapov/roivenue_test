377b67e2c234a7d817dffe2ca688bd60ac08574d
Maksim Krivolapov <givago@mail.ru>
2021-02-23 22:27:57 +0100

    current version of budget optimizer class

--- BudgetOptimizer.py
+++ BudgetOptimizer.py
@@ -1,54 +1,311 @@
-#import pandas as pd
+import pandas as pd
 #import math
-import matplotlib.pyplot as plt
-#import numpy as np
-#from scipy.stats import kurtosis, iqr, shapiro
-#from scipy.optimize import differential_evolution
-#import warnings
 #import matplotlib.pyplot as plt
+import numpy as np
+#from scipy.stats import kurtosis, iqr, Shapiro
+from scipy.optimize import differential_evolution
+import warnings
+#import matplotlib.pyplot as plt
+import scipy.stats as stat
 
-from Budget_optimiser_function import filter_df
+#from Budget_optimiser_function import filter_df
 
 settings_estimator = {'alpha'   :   0.1,
-                      'corr_thd':   0.8,
+                      'corr_thd':   0.9,
                       'time_window_regr':12,
                       'period'  :   'week',
                       'margin'  :   1.0,
                       'level'   :   'platform',
                       'dependant_var':'grossProfit',
                       'independatnt_var': 'marketingInvestment',
+                      'filter_type': 'EWM',
                       'client': ''
                       }
 
+DateTimeID = ['periodStartDate']
+
+usecols_ = [
+    "account","adCampaign", "platformCode","currencyCode", "channelCode", "marketingInvestment",
+    "impressions", "clicks", "visits", "conversions", "deliveries",
+    "netRevenue", "grossProfit", "adGroup", "periodStartDate","businessUnit"
+          ]
+
+dtype_ = {
+    'account': str,
+    'adCampaign' : str,
+    'platformCode' : str,
+    'businessUnit':str,
+    'currencyCode':str,
+    'channelCode' : str,
+    'impressions' : int,
+    'clicks' : int,
+    'visits' : int,
+    'conversions' : int,
+    'deliveries' : int,
+    'netRevenue' : float,
+    'grossProfit' : float,
+    'adGroup' : str,
+    'marketingInvestment': float
+}
+
+stat_name = ['Mean','Std','Med','Sqew','Kurtosis','Var','Iqr','Shapiro-stat','Shapiro-Pr']
+regr_name = ['a','b','c','a-err','b-err','c-err']
+r2_stat_name = ['R2-mean','R2-std','R2-norm','R2-chi-sqr'] # func [line, log] if line -> c=NaN
+settings_optimizer = {}
+
+settings_trainer = {}
+
 class EstimatorClass:
+
     def __init__(self):
         self.dataset_size = 0
-        pass
+        print("Init estimator class")
+
 
     def load_settings(self, settings):
         self.settings = settings
-        pass
+        print("Load Settings")
+
 
     def load_data(self, dataframe):
         self.data = dataframe
         self.dataset_size = len(self.data)
-        print(type(self.data))
+        print("Load data")
 
-    def filter_data(self):
-        df = self.data
-        self.filtered_data = filter_df(df, self.settings['alpha'])
-        pass
 
-    def corr_data(self, data):
-        pass
+    def regr_calc(self):
+        print('Regression method')
+
+
+    def corr_data(self):
+        #df = self.data
+
+        def filter_df(df, alpha):
+            return df.ewm(alpha = alpha, adjust = False).mean()
+
+
+        def select_agg_resample_df(df, index, granularity, use_nan):
+            """Function for select and group  data in dataframe
+            input: pandas dataframe, name of index, granularity['week','month'] default as in dataframe
+            return: aggregated dataframe with extra column with indexes [ROI, CPI, CPC]"""
+            group_df = df.groupby(['periodStartDate']).agg({
+            'netRevenue': 'sum','marketingInvestment': 'sum', 'visits': 'sum', 'conversions': 'sum',
+            'deliveries': 'sum', 'impressions': 'sum', 'clicks': 'sum', 'grossProfit': 'sum' })   
+
+            if granularity == 'week':
+                group_agg_df = group_df.resample('W-MON').agg('sum')
+            elif granularity == 'month':
+                group_agg_df = group_df.resample('M', convention='end').agg('sum')
+            elif granularity == 'day':
+                group_agg_df = group_df
+            else:
+                raise ValueError("Incorrect aggregation period, shuld be 'day', 'week' or 'month'")
+
+            group_agg_df['ROI'] = (group_agg_df['netRevenue']/group_agg_df['marketingInvestment'])
+            group_agg_df['CPI'] = (group_agg_df['marketingInvestment']/group_agg_df['impressions'])
+            group_agg_df['CPC'] = (group_agg_df['marketingInvestment']/group_agg_df['clicks'])
+            group_agg_df['POI'] = (group_agg_df['marketingInvestment']/group_agg_df['grossProfit']) # Profit over investment
+
+            if use_nan == False:
+                group_agg_df.fillna(0)
+            else:
+                pass
+            return group_agg_df
+
+
+        def log_f(x, a, b):
+            return a * (1-np.exp(-x/b)) #a * (1 - np.exp((x/b)))
+
+
+        # function for genetic algorithm to minimize (sum of squared error)
+        def sumOfSquaredError(parameterTuple):
+            function = log_f # it's bad staff #TODO
+            warnings.filterwarnings("ignore") # do not print warnings by genetic algorithm
+            val = function(t_train, *parameterTuple)
+            return np.sum((y_train - val) ** 2.0)
+
+
+        def generate_Initial_Parameters(t_train, y_train,function):
+            """ Calculate intitial paramiters for curve_fit function"""
+            # min and max used for bounds
+            maxX = max(t_train)
+            #minX = min(t_train)
+            maxY = max(y_train)
+            #minY = min(y_train)
+            maxXY = max(maxX, maxY)
+
+            parameterBounds = []
+            parameterBounds.append([-maxXY, maxXY]) # search bounds for a
+            parameterBounds.append([-maxXY, maxXY]) # search bounds for b
+            #parameterBounds.append([-maxXY, maxXY]) # search bounds for c
+
+            # "seed" the numpy random number generator for repeatable results
+            result = differential_evolution(sumOfSquaredError, parameterBounds, seed=3)
+            return result.x
+
+
+        def regression_calc(df, function):
+            """Calculate paramiters of regression function
+            input: pandas dataframe [x,y]
+            output: list[paramiters], list[std diviation err of paramiters]"""
+            from scipy.optimize import curve_fit
+            t_train = df[df.columns[0]]  #
+            y_train = df[df.columns[1]]
+            geneticParameters = generate_Initial_Parameters(t_train, y_train, function)
+            popt, pcov = curve_fit(function, t_train, y_train, geneticParameters)
+
+            # class method return
+            error  = np.sqrt(np.diag(pcov))
+            return popt, error    
+ 
+
+        business_unit_code = self.data['businessUnit'].unique().tolist()
+        business_unit_code.sort()
+        channel_code_code = self.data['channelCode'].unique().tolist()
+        channel_code_code.sort()
+        currency_code_code = self.data['currencyCode'].unique().tolist()
+        currency_code_code.sort()
+        account_code = self.data['account'].unique().tolist()
+
+
+        df_time_selected = self.data[self.data['periodStartDate'] >= self.data['periodStartDate'].max() - pd.DateOffset(weeks=12)]
+
+        output = pd.DataFrame()
+
+        for i in business_unit_code:
+    
+    
+            df_selected = df_time_selected[df_time_selected['businessUnit'] == i]
+   
+            if self.settings['level'] == 'platform':
+                component_code = df_selected['platformCode'].unique().tolist()
+        
+            elif self.settings['level'] == 'compaign':
+                component_code = df_selected['adCampaign'].unique().tolist()
+        
+            elif self.settings['level'] == 'channel':
+                component_code = df_selected['channelCode'].unique().tolist()
+    
+            else:
+                print('Incorrect component settings')
+        
+            for z in component_code:
+                temp_dict = dict([('Client', ''), ('Business_unit', ''), ('Platform', ''),
+                          ('Corr_coeff', 0), ('Pr_value', 0), ('a', 0), ('b', 0),
+                          ('Mean', 0), ('R_std', 0), ('Invest', 0), ('Profit', 0),
+                          ('Prof_last_week', 0), ('Invest_last_week', 0),
+                          ('Prof_prev_week', 0), ('Invest_prev_week', 0),
+                          ('Max_week_profit', 0), ('Max_week_invest', 0),
+                          ('X',[]),('Y',[]),('Regr_X',[]),('Regt_Y',[])])
+        
+        
+                if self.settings['level'] == 'platform':
+                    df_platform = df_selected[df_selected['platformCode'] == z]
+        
+                elif self.settings['level'] == 'compaign':
+                    df_platform = df_selected[df_selected['adCampaign'] == z]
+        
+        
+                elif self.settings['level'] == 'channel':
+                    df_platform = df_selected[df_selected['channelCode'] == z]
+        
+
+                df_business_selected_agg = select_agg_resample_df( df_platform, 'periodStartDate', 'week', use_nan=False )
+               
+                temp_dict['Max_week_profit'] = df_business_selected_agg.grossProfit[:].max()
+                temp_dict['Max_week_invest'] = df_business_selected_agg.marketingInvestment[:].max()
+        
+                temp_dict['Prof_prev_week'] = df_business_selected_agg.grossProfit[-2:-1].max()
+                temp_dict['Invest_prev_week'] = df_business_selected_agg.marketingInvestment[-2:-1].max()
+        
+                temp_dict['Prof_last_week'] = df_business_selected_agg.grossProfit[-1].max()
+                temp_dict['Invest_last_week'] = df_business_selected_agg.marketingInvestment[-1].max()
+        
+                df_business_selected_agg = df_business_selected_agg.iloc[:-1, :] # get rid of last week in dataset     
+        
+                if self.settings['filter_type'] == 'EWM':
+                    df_business_selected_agg_filter = filter_df( df_business_selected_agg, self.settings['alpha'] )
+            
+                elif self.settings['filter_type'] == 'None':
+                    df_business_selected_agg_filter = df_business_selected_agg
+            
+                elif self.settings['filter_type'] == 'Median':
+                    print("Median Filter have Not implemented yet") 
+        
+                else:
+                    print("Incorrect Filter settings") 
+
+                temp_dict['Client'] = account_code[0]
+                temp_dict['Business_unit'] = i
+                temp_dict['Component'] = str(i) + '/' + str(z)
+
+                temp_dict['Profit'] = np.round( df_business_selected_agg['grossProfit'].sum(), 2 )
+                temp_dict['Invest'] = np.round( df_business_selected_agg['marketingInvestment'].sum(), 2)
+
+                sper_crr, P_value = stat.spearmanr( df_business_selected_agg_filter['marketingInvestment'], df_business_selected_agg_filter['grossProfit'])
+                temp_dict['Corr_coeff'] = sper_crr
+                temp_dict['Pr_value'] = P_value
+
+
+                sorted_df = df_business_selected_agg_filter
+        
+                temp_dict['Mean'] = sorted_df['grossProfit'].describe()[1]
+        
+                t_train = sorted_df['marketingInvestment']
+                y_train = sorted_df['grossProfit']
+        
+                temp_dict['X'] = t_train.to_list()
+                temp_dict['Y'] = y_train.to_list()
+
+        
+                if sper_crr > 0.9:
+                    #print(i + ' business unit ' + z + ' platform has \t' + str(sper_crr) + ' corr coefficient')
+                    try:
+
+                        regr, err = regression_calc(sorted_df[['marketingInvestment', 'grossProfit']], log_f)
+
+                        R = sorted_df['grossProfit'] -  log_f(sorted_df['marketingInvestment'], *regr)
+                        R_std = R.describe()['std']
+
+                        a, b = regr
+                        temp_dict['a'] = a
+                        temp_dict['b'] = b
+                        temp_dict['R_std'] = R_std
+
+                        # add 25% to maximum value for visualisation of the prediction (+-10 or 20%)
+                        x_space = np.linspace(0, (max(temp_dict['X']) * 1.5), num = len(temp_dict['X']))
+                        temp_dict['Regr_X'] = x_space.tolist() 
+                        temp_dict['Regt_Y'] = log_f(x_space, a, b)
+                            
+           
+                    except:
+                        print("Regression wasn't calculated")
+                        temp_dict['a'] = 0
+                        temp_dict['b'] = 0
+                else:
+                    temp_dict['a'] = 0
+                    temp_dict['b'] = 0
+
+                output = output.append(temp_dict, ignore_index=True)
+        
+        
+        print("Correlation calculation executed")
+
+
+        return output
 
 
 class OptimizerClass:
     def __init__(self):
-        pass
+        print("Optimizer class Init")
 
     def load_settings(self, settings):
         self.settings = settings
+        print("Load optimizer settings")
+
+    def load_data(self, data):
+        self.data = data
+        print("Load optimizer data")
 
 
 class TrainerClass:
--- BudgetOptimizer_tb.py
+++ BudgetOptimizer_tb.py
@@ -6,14 +6,42 @@ Created on Mon Feb  8 16:38:43 2021
 """
 
 from BudgetOptimizer import EstimatorClass, settings_estimator
+
+
 import pandas as pd
-from random import random
+#pd.set_option()
+import sys
+import json
+
+encoding = sys.getdefaultencoding()
+import os
+import numpy as np
+import xlsxwriter
+from pathlib import Path
 
 
-data = pd.DataFrame([random() for i in range(20)])
+pd.set_option('display.max_columns', None)
+pd.options.display.float_format = '{:,.3f}'.format
+
+from BudgetOptimizer import EstimatorClass, settings_estimator
+from BudgetOptimizer import DateTimeID, dtype_, stat_name, regr_name, r2_stat_name, usecols_
+
+list_files = os.listdir("./input")
+mypath = Path().absolute()
+folder = 'budget_opt\\'
+img_folder = folder + 'img\\'
+
+#################################################################################
+
+df = pd.read_csv('input\\'+ list_files[0], sep="\t", infer_datetime_format=True,
+                 usecols=usecols_, dtype=dtype_, parse_dates=DateTimeID)
+
+df['periodStartDate'] = pd.to_datetime(df['periodStartDate'])
 
 estimator = EstimatorClass()
 
 estimator.load_settings(settings_estimator)
 
-estimator.load_data(data)
+estimator.load_data(df)
+
+result = estimator.corr_data()
